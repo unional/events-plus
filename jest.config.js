b378659023cded60eb9c4c686040956a4a39d91d
unional <homawong@gmail.com>
2022-04-23 01:32:37 -0700

    chore: test EventTarget only after node 14

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,6 @@
+const base = require('./config/jest.config.base')
+
 module.exports = {
-  'collectCoverageFrom': [
-    '<rootDir>/ts/**/*.[jt]s'
-  ],
-  'reporters': [
-    'default',
-    'jest-progress-tracker'
-  ],
-  'roots': [
-    '<rootDir>/ts',
-  ],
-  'testEnvironment': 'node',
-  'testMatch': ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
-  'watchPlugins': [
-    'jest-watch-suspend',
-    'jest-watch-repeat',
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-    [
-      'jest-watch-toggle-config', { 'setting': 'verbose' },
-    ],
-    [
-      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
-    ],
-  ],
+  ...base,
+  testEnvironment: 'node'
 }
