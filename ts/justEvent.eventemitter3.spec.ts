f1c9bea3e955d53710f2450f66f5a1e381e55770
unional <homawong@gmail.com>
2022-05-12 12:49:57 -0700

    feat: add listenTo emitBy
    BREAKING CHANGE: rename handle to listener
    to better describe what it does,
    and make the terms more consistent with listenTo and emitBy

--- ts/justEvent.eventemitter3.spec.ts
+++ ts/justEvent.eventemitter3.spec.ts
@@ -6,7 +6,16 @@ test('usage', () => {
 
   const count = justEvent<number>('count')
 
-  emitter.addListener(count.type, count.handle(value => expect(value).toBe(1)))
+  emitter.addListener(count.type, count.listener(value => expect(value).toBe(1)))
 
   emitter.emit(count.type, ...count(1))
+})
+
+it('takes emitter for listenTo and emitBy', () => {
+  const emitter = new EventEmitter()
+
+  const count = justEvent<number>('count')
+
+  count.listenTo(emitter, value => expect(value).toBe(1))
+  count.emitBy(emitter, 1)
 })
