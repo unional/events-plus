524cfaf9bafb5d8fd042882bac3b34efcf52f0fe
Homa Wong <homawong@gmail.com>
2022-06-15 17:42:19 -0700

    chore: use yarn@3 and update deps

--- ts/trapError.eventemitter3.spec.ts
+++ ts/trapError.eventemitter3.spec.ts
@@ -1,7 +1,5 @@
 import { EventEmitter } from 'eventemitter3'
-import { beforeEachTrapErrorTest, testTrapError, thrower } from './trapErrorTest'
-
-beforeEachTrapErrorTest()
+import { testTrapError, thrower } from './trapErrorTest'
 
 describe('trapError', () => {
   test('trap error from `addListener()`', () => {
